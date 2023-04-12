5813a85452d698d7467a1c8668179706e45ed6e4
ilya <User@WIN-ISS673VO7OO>
2023-04-12 15:32:40 +0300

    Добавьте файлы проекта.

---
+++ Task_3_PolessUP/Program.cs
@@ -0,0 +1,29 @@
+﻿//Учитывая отсортированный массив различных целых чисел и целевое значение, вернуть индекс, если цель найдена. Если нет, верните индекс туда, где он был бы, если бы он был вставлен по порядку. 
+//Ввод: числа = [1, 3, 5, 6], цель = 5
+//Вывод: 2
+using System.Formats.Asn1;
+
+int[] array = { 1, 3, 5, 6 };
+int target = 2;
+bool res = false;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == target)
+    {
+        res = true;
+        Console.WriteLine(i);
+    }
+    
+    
+}
+
+if (res == false)
+{
+    for (int i = 0;i < array.Length; i++)
+    {
+        if (target > array[i] && target< array[i+1] && i+1<array.Length)
+        {
+            Console.WriteLine(i+1);
+        }
+    }
+}
